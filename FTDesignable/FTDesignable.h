fdd1414b1e536749fd672e40dcdea47f2e2abee3
Frank Thamel <w.frankthamel@gmail.com>
2017-07-19 09:49:52 +0530

    initial project commit

---
+++ FTDesignable/FTDesignable.h
@@ -0,0 +1,19 @@
+//
+//  FTDesignable.h
+//  FTDesignable
+//
+//  Created by Frank Thamel on 7/19/17.
+//  Copyright © 2017 co.talene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTDesignable.
+FOUNDATION_EXPORT double FTDesignableVersionNumber;
+
+//! Project version string for FTDesignable.
+FOUNDATION_EXPORT const unsigned char FTDesignableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTDesignable/PublicHeader.h>
+
+
